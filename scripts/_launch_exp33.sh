b3782b38538bf322df8667a1188a0b1a02070284
philcuriosity1024 <philcuriosity1024@gmail.com>
2026-07-03 16:27:49 +0800

    移除 scripts 目录

--- scripts/_launch_exp33.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/heqi/project-qat/ultralytics
-exec /home/heqi/miniforge3/envs/torch2.6-qat-yolo/bin/python -u train.py >> /tmp/exp33.log 2>&1
--- scripts/_launch_seg.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/heqi/project-qat/ultralytics
-exec /home/heqi/miniforge3/envs/torch2.6-qat-yolo/bin/python -u train-seg.py >> /tmp/exp1-seg.log 2>&1
