35d2f8fc47a6ec9626c231a4b64db96e85af059b
philcuriosity1024 <philcuriosity1024@gmail.com>
2026-08-03 17:16:49 +0800

    sync(dev): update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -8,7 +8,7 @@ from copy import deepcopy
 from pathlib import Path
 
 import torch
-from torch import nn
+import torch.nn as nn
 
 from ultralytics.nn.autobackend import check_class_names
 from ultralytics.nn.modules import (
@@ -1380,9 +1380,11 @@ class SafeClass:
 
     def __init__(self, *args, **kwargs):
         """Initialize SafeClass instance, ignoring all arguments."""
+        pass
 
     def __call__(self, *args, **kwargs):
         """Run SafeClass instance, ignoring all arguments."""
+        pass
 
 
 class SafeUnpickler(pickle.Unpickler):
