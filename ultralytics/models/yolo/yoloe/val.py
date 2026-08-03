432f1bfed37e3e20ab1856e6562ec5d3cb3c352e
philcuriosity1024 <philcuriosity1024@gmail.com>
2026-08-03 17:16:46 +0800

    sync(dev): update val.py

--- ultralytics/models/yolo/yoloe/val.py
+++ ultralytics/models/yolo/yoloe/val.py
@@ -202,3 +202,5 @@ class YOLOEDetectValidator(DetectionValidator):
 
 class YOLOESegValidator(YOLOEDetectValidator, SegmentationValidator):
     """YOLOE segmentation validator that supports both text and visual prompt embeddings."""
+
+    pass
